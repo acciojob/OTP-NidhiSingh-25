37e16decbcace92059c7bfb5c2e544ba8febf535
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-21 15:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 //your JS code here. If required.
-const inputs = document.querySelectorAll('.code');
+ const inputs = document.querySelectorAll('.code');
 
-inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {
-    const value = input.value;
-    if (value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    }
-  });
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      if (input.value === '') {
-        if (index > 0) {
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && !input.value && index > 0) {
           inputs[index - 1].focus();
           inputs[index - 1].value = '';
         }
-      } else {
-        input.value = '';
-      }
-    }
-  });
-});
+      });
+    });
+
+    // Auto focus on first input on load
+    inputs[0].focus();
